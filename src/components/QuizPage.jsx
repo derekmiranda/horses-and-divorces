a03e39387b2f3e4ba17068b8de88b311a34b2980
Derek Miranda <derek.miranda@hearst.com>
2025-10-04 13:09:43 -0400

    disable Timer for now

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,5 +1,4 @@
 import "./QuizPage.css"
-import useCountdown from "../hooks/useCountdown";
 
 function PersonSection({ children }) {
   return (
@@ -11,26 +10,9 @@ function PersonSection({ children }) {
 }
 
 function QuizPage() {
-  const { secondsLeft, isRunning, pause, start, reset } = useCountdown(30, {
-    autoStart: false,
-    onExpire: () => console.log("time's up"),
-  });
-
   return (
     <>
       <h1>horses and divorces / wiki-exes</h1>
-        <div className="timer-row">
-          <div className="timer">{secondsLeft}s</div>
-          <div className="timer-controls">
-            {isRunning ? (
-              <button onClick={pause}>Pause</button>
-            ) : (
-              <button onClick={start}>Start</button>
-            )}
-            <button onClick={() => reset(30)}>Reset</button>
-          </div>
-        </div>
-
       <section className="quiz-row">
         <PersonSection>this one?</PersonSection>
         <p>who has the most divorces?</p>
