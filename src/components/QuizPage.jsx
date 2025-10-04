4d866b738e2587d2a02308d52715daa86d51725c
Derek Miranda <derek.miranda@hearst.com>
2025-10-04 12:41:06 -0400

    add basic quiz page

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,10 +1,10 @@
 import "./QuizPage.css"
 
-function PersonSection() {
+function PersonSection({ children }) {
   return (
     <div>
-      <div className="person-"></div>
-      <button>this one?</button>
+      <div className="person-img"></div>
+      <button>{children}</button>
     </div>
   )
 }
@@ -13,9 +13,11 @@ function QuizPage() {
   return (
     <>
       <h1>horses and divorces / wiki-exes</h1>
-      <PersonSection />
+      <main className="quiz-page">
+      <PersonSection>this one?</PersonSection> 
       <p>who has the most divorces?</p>
-      <PersonSection />
+      <PersonSection>or this one?</PersonSection>
+      </main>
     </>
   )
 }
